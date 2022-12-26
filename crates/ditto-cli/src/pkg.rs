e0547466e9da822f184b7c143be515b8521955f8
Jordan Mackie <jmackie@protonmail.com>
2022-12-26 16:19:51 +0000

    Add support for github packages

--- crates/ditto-cli/src/pkg.rs
+++ crates/ditto-cli/src/pkg.rs
@@ -1,9 +1,12 @@
 // Maybe this should live in it's own crate?
-use crate::{common::is_plain, spinner::Spinner};
+use crate::{
+    common::{get_ditto_cache_dir, is_plain},
+    spinner::Spinner,
+};
 use console::{Emoji, Style};
 use ditto_config::{
-    read_config, Config, Dependencies, PackageName, PackageSetPackages as Packages, PackageSpec,
-    CONFIG_FILE_NAME,
+    read_config, Config, Dependencies, GithubPackageSpec, PackageName,
+    PackageSetPackages as Packages, PackageSpec, CONFIG_FILE_NAME,
 };
 use indicatif::MultiProgress;
 use log::{debug, warn};
@@ -77,7 +80,8 @@ pub async fn check_packages_up_to_date(
         &mut Dependencies::new(),
         &installed_packages,
         &available_packages,
-    )?;
+    )
+    .await?;
 
     debug!(
         "Updating {} with {}",
@@ -106,8 +110,8 @@ fn hash_packages_inputs(dependencies: &Dependencies, packages: &Packages) -> u64
     hasher.finish()
 }
 
-// TODO make this async
-fn update_dependencies(
+#[async_recursion::async_recursion]
+async fn update_dependencies(
     _multi_progress: &mut MultiProgress,
     packages_dir: &Path,
     dependencies: &Dependencies,
@@ -132,7 +136,7 @@ fn update_dependencies(
                     debug!("Removing existing install of {}", dependency.as_str());
                     spinner.set_message("removing existing install");
                     remove_package(packages_dir, dependency)?;
-                    install_package(spinner, packages_dir, dependency, available_spec)?;
+                    install_package(spinner, packages_dir, dependency, available_spec).await?;
                 }
                 updated_dependencies.insert(dependency.clone());
                 let config = read_package_config(packages_dir, dependency)?;
@@ -143,7 +147,8 @@ fn update_dependencies(
                     updated_dependencies,
                     installed_packages,
                     available_packages,
-                )?
+                )
+                .await?
             }
             (None, Some(available_spec)) => {
                 // Not installed
@@ -151,7 +156,7 @@ fn update_dependencies(
                 //let mut progress = multi_progress.add(ProgressBar::new_spinner());
                 let spinner = Spinner::new_with_prefix(dependency.as_str().to_string());
 
-                install_package(spinner, packages_dir, dependency, available_spec)?;
+                install_package(spinner, packages_dir, dependency, available_spec).await?;
                 updated_dependencies.insert(dependency.clone());
                 let config = read_package_config(packages_dir, dependency)?;
                 update_dependencies(
@@ -161,7 +166,8 @@ fn update_dependencies(
                     updated_dependencies,
                     installed_packages,
                     available_packages,
-                )?
+                )
+                .await?
             }
             (Some(_installed_spec), None) => {
                 return Err(miette!(
@@ -179,47 +185,185 @@ fn update_dependencies(
 
 const EXTENSION_SPEC: &str = "spec";
 
-fn install_package(
-    mut spinner: Spinner,
+async fn install_package(
+    spinner: Spinner,
     packages_dir: &Path,
     package_name: &str,
     spec: &PackageSpec,
 ) -> Result<()> {
-    debug!("Installing {:?}", package_name);
+    debug!("installing {:?}", package_name);
     match spec {
-        PackageSpec::Path { path: src } => {
-            let mut dst = packages_dir.to_path_buf();
-            let src = pathdiff::diff_paths(src, packages_dir).unwrap();
-            dst.push(package_name);
-
-            debug!(
-                "linking {} -> {}",
-                dst.to_string_lossy(),
-                src.to_string_lossy(),
-            );
-            spinner.set_message(format!(
-                "{} -> {}",
-                dst.to_string_lossy(),
-                src.to_string_lossy(),
-            ));
-            symlink::symlink_dir(src, dst).into_diagnostic()?;
-
-            let mut spec_path = packages_dir.to_path_buf();
-            spec_path.push(package_name);
-            spec_path.set_extension(EXTENSION_SPEC);
-            let spec_file = fs::File::create(&spec_path).into_diagnostic()?;
-            serde_json::to_writer(spec_file, spec).into_diagnostic()?;
-
-            debug!(
-                "{:?} spec written to {}",
+        PackageSpec::Path { path } => {
+            install_package_from_path(spinner, packages_dir, package_name, path)?;
+        }
+        PackageSpec::Github {
+            github,
+            revision,
+            sha256,
+        } => {
+            install_package_from_github(
+                spinner,
+                packages_dir,
                 package_name,
-                spec_path.to_string_lossy()
-            );
-
-            spinner.success("installed")
+                github,
+                revision,
+                sha256,
+            )
+            .await?;
         }
     }
+
+    let mut spec_path = packages_dir.to_path_buf();
+    spec_path.push(package_name);
+    spec_path.set_extension(EXTENSION_SPEC);
+    let spec_file = fs::File::create(&spec_path).into_diagnostic()?;
+    serde_json::to_writer(spec_file, spec).into_diagnostic()?;
+
+    debug!(
+        "{:?} spec written to {}",
+        package_name,
+        spec_path.to_string_lossy()
+    );
+
     Ok(())
+}
+
+fn install_package_from_path(
+    mut spinner: Spinner,
+    packages_dir: &Path,
+    package_name: &str,
+    path: &Path,
+) -> Result<()> {
+    let src = pathdiff::diff_paths(path, packages_dir).unwrap();
+    let mut dst = packages_dir.to_path_buf();
+    dst.push(package_name);
+
+    debug!(
+        "linking {} -> {}",
+        dst.to_string_lossy(),
+        src.to_string_lossy(),
+    );
+    spinner.set_message(format!(
+        "{} -> {}",
+        dst.to_string_lossy(),
+        src.to_string_lossy(),
+    ));
+    symlink::symlink_dir(src, dst).into_diagnostic()?;
+
+    spinner.success("installed");
+
+    Ok(())
+}
+
+async fn install_package_from_github(
+    mut spinner: Spinner,
+    packages_dir: &Path,
+    package_name: &str,
+    github: &GithubPackageSpec,
+    revision: &str,
+    sha256: &str,
+) -> Result<()> {
+    let mut zip_archive =
+        fetch_cached_github_archive(&mut spinner, github, revision, sha256).await?;
+
+    let mut dst = packages_dir.to_path_buf();
+    dst.push(package_name);
+
+    for name in zip_archive
+        .file_names()
+        .map(|str| str.to_string())
+        .collect::<Vec<_>>()
+    {
+        let mut zip_file = zip_archive.by_name(&name).into_diagnostic()?;
+        if zip_file.is_dir() {
+            continue;
+        }
+        let zip_path = zip_file
+            .enclosed_name()
+            .ok_or_else(|| miette!("encountered bad zip file path!"))?;
+        let mut dst = dst.clone();
+        dst.extend(
+            // drop the root directory from the zip file
+            zip_path.components().skip(1),
+        );
+
+        if let Some(parent) = dst.parent() {
+            if !parent.exists() {
+                fs::create_dir_all(parent).into_diagnostic()?;
+            }
+        }
+        debug!("{:?} -> {:?}", zip_path, &dst);
+        let mut file = std::fs::File::create(dst).into_diagnostic()?;
+        std::io::copy(&mut zip_file, &mut file).into_diagnostic()?;
+    }
+
+    spinner.success("installed");
+    Ok(())
+}
+
+async fn fetch_cached_github_archive(
+    spinner: &mut Spinner,
+    github: &GithubPackageSpec,
+    revision: &str,
+    sha256: &str,
+) -> Result<zip::ZipArchive<std::fs::File>> {
+    // Is it in the cache?
+    let mut cached_zip = get_github_cache_dir()?;
+    cached_zip.push(sha256);
+    cached_zip.set_extension("zip");
+    if cached_zip.exists() {
+        let file = std::fs::File::open(&cached_zip).into_diagnostic()?;
+        let zip_archive = zip::ZipArchive::new(file).into_diagnostic()?;
+        return Ok(zip_archive);
+    }
+
+    // Nope, download it...
+    let archive_url = format!(
+        "https://github.com/{owner}/{repo}/archive/{revision}.zip",
+        owner = github.owner,
+        repo = github.repo,
+    );
+    debug!("GET {}", archive_url);
+    spinner.set_message(format!("Downloading {}", archive_url));
+    let response = reqwest::get(&archive_url).await.into_diagnostic()?;
+    if !response.status().is_success() {
+        return Err(miette!("{} {}", response.status(), archive_url));
+    }
+    let content = response.bytes().await.into_diagnostic()?;
+    let got_sha256 = sha256::digest(content.as_ref());
+    if got_sha256 != sha256 {
+        return Err(miette!(
+            "sha256 mismatch for {}/{}, expected {:?} but got {:?}",
+            github.owner,
+            github.repo,
+            sha256,
+            got_sha256
+        ));
+    }
+    debug!("Writing archive to {:?}", cached_zip);
+    std::fs::write(&cached_zip, content).into_diagnostic()?;
+    let file = std::fs::File::open(&cached_zip).into_diagnostic()?;
+    let zip_archive = zip::ZipArchive::new(file).into_diagnostic()?;
+    Ok(zip_archive)
+}
+
+pub fn get_github_cache_dir() -> Result<PathBuf> {
+    let mut cache_dir = get_ditto_cache_dir()?;
+    cache_dir.push("github");
+    if !cache_dir.exists() {
+        debug!(
+            "Github cache directory doesn't exist, creating {:?}",
+            cache_dir
+        );
+        std::fs::create_dir_all(&cache_dir)
+            .into_diagnostic()
+            .wrap_err(format!(
+                "error initializing github cache dir at {:?}",
+                cache_dir
+            ))?;
+    }
+
+    Ok(cache_dir)
 }
 
 fn remove_package(packages_dir: &Path, package_name: &str) -> Result<()> {
