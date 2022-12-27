54a5a2da736c0bca9812426b59f256c160e1c947
Jordan Mackie <jmackie@protonmail.com>
2022-12-27 09:48:22 +0000

    Support DITTO_CACHE_DIR environment variable

--- crates/ditto-cli/src/common.rs
+++ crates/ditto-cli/src/common.rs
@@ -3,8 +3,14 @@ use miette::{miette, IntoDiagnostic, Result, WrapErr};
 use std::path::PathBuf;
 
 pub fn get_ditto_cache_dir() -> Result<PathBuf> {
-    let mut cache_dir = dirs::cache_dir().ok_or_else(|| miette!("Error getting cache dir"))?;
-    cache_dir.push("ditto");
+    let cache_dir = if let Ok(cache_dir) = std::env::var("DITTO_CACHE_DIR") {
+        PathBuf::from(cache_dir)
+    } else {
+        let mut cache_dir =
+            dirs::cache_dir().ok_or_else(|| miette!("Error getting standard cache dir"))?;
+        cache_dir.push("ditto");
+        cache_dir
+    };
     if !cache_dir.exists() {
         debug!("Cache directory doesn't exist, creating {:?}", cache_dir);
         std::fs::create_dir_all(&cache_dir)
